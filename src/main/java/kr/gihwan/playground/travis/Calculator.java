ff51d89814d78e5bed5cd30ef309059ed5397265
HanQ <38851923+Shin-Han-Gyu@users.noreply.github.com>
2019-02-27 19:28:55 +0900

    commit

---
+++ src/main/java/kr/gihwan/playground/travis/Calculator.java
@@ -0,0 +1,22 @@
+package kr.gihwan.playground.travis;
+
+public class Calculator {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0.0d) {
+            throw new IllegalArgumentException("divisor must not be zero.");
+        }
+        return a / b;
+    }
+}
